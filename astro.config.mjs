8ef7c47d6afeb8074186e694ce0399d0a2f968e1
elieagencekano <elie@consultingkano.fr>
2025-11-10 20:38:47 +0100

    site prévi github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://elieagencekano.github.io/kano_website/",
+  base: "/kano_website/",
+  outDir: "./dist",
+});
