f5b432c47fb96b920b45d1c2146b2486799c9604
elieagencekano <elie@consultingkano.fr>
2025-11-13 18:11:40 +0100

    fix img src

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+const isBlitz =
+  typeof process !== "undefined" &&
+  process.env?.STACKBLITZ_ENV === "true";
+
 export default defineConfig({
   site: "https://elieagencekano.github.io/kano_website/",
-  base: "/kano_website/",
-  output: "static"
+  base: isBlitz || import.meta.env.DEV ? "/" : "/kano_website/",
+  output: "static",
 });
