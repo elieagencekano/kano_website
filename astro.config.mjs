5490bc1f8b2571a0cbe3b02dcb00f07d521a3fcb
elieagencekano <elie@consultingkano.fr>
2025-11-13 17:11:39 +0100

    astrconfig fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-import staticAdapter from "@astrojs/static";
 
 const base = import.meta.env.DEV ? "/" : "/kano_website/";
 
 export default defineConfig({
   site: "https://elieagencekano.github.io",
-  base: base,
-  output: "static",
-  adapter: staticAdapter(),
+  base,
+  output: "static"
 });
