2fd7349c721f653bfe9860a93098090bf9d173f6
elieagencekano <elie@consultingkano.fr>
2026-02-04 15:44:09 +0100

    update carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-
-const isBlitz =
-  typeof process !== "undefined" &&
-  process.env?.STACKBLITZ_ENV === "true";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://elieagencekano.github.io/kano_website/",
-  base: isBlitz || import.meta.env.DEV ? "/" : "/kano_website/",
+  site: "https://consultingkano.fr",
+  base: "/",
   output: "static",
+  integrations: [sitemap()]
 });
